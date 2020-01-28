9d8b412df7a280cf10c4fbbe859d45d5f04161c1
Eirik Brandtzaeg <eirikb@eirikb.no>
2020-01-28 12:14:44 +0100

    Update auth flow
    MSAL is picking up the hash and redirecting to "itself".
    We don't want this, we want to pick up the hash ourselves and pass it to main frame.

--- app.js
+++ app.js
@@ -5,46 +5,61 @@ import { UserAgentApplication } from 'msal';
 
 const clientId = process.env.CLIENTID;
 const scopes = ['User.Read'];
-const msal = new UserAgentApplication({
-  auth: { clientId },
-  cache: { cacheLocation: 'localStorage' }
-});
 
 const p = document.querySelector('p');
 const loginButton = document.querySelector('#login');
 const clearCacheButton = document.querySelector('#clear-cache');
 
-clearCacheButton.addEventListener('click', () => {
-  msal.clearCache();
-});
-
 function log(text) {
   p.innerText = JSON.stringify(text);
 }
 
-async function callApi() {
-  log('Call Graph API (get /me)');
-  try {
-    const { accessToken } = await msal.acquireTokenSilent({ scopes });
-    log(accessToken);
-    const me = await fetch('https://graph.microsoft.com/v1.0/me', {
-      headers: {
-        authorization: `Bearer ${accessToken}`
+(() => {
+  if (window.location.hash.includes('id_token=')) {
+    log('Id token in hash (is callback)');
+    Office.onReady(() => {
+      log('Window is callback (wait)');
+      if (Office.context.ui) {
+        Office.context.ui.messageParent(window.location.hash);
+        log('Message sent to parent');
+      } else {
+        log('Missing Office.context.ui');
       }
-    }).then(res => res.json());
-    log(me);
-  } catch (e) {
-    log(e);
-    throw e;
+    });
+    return;
   }
-}
 
-log('Waiting for office...');
-Office.onReady(() => {
-  if (msal.isCallback(window.location.hash)) {
-    log('Window is callback (wait)');
-    Office.context.ui.messageParent(window.location.hash);
-  } else {
+  // Only initialize msal if window is not callback.
+  // MSAL will pick up the hash and redirect - we don't want this.
+  const msal = new UserAgentApplication({
+    auth: { clientId },
+    cache: { cacheLocation: 'localStorage' }
+  });
+
+  clearCacheButton.addEventListener('click', () => {
+    msal.clearCache();
+  });
+
+  async function callApi() {
+    log('Call Graph API (get /me)');
+    try {
+      const { accessToken } = await msal.acquireTokenSilent({ scopes });
+      log(accessToken);
+      const me = await fetch('https://graph.microsoft.com/v1.0/me', {
+        headers: {
+          authorization: `Bearer ${accessToken}`
+        }
+      }).then(res => res.json());
+      log(me);
+    } catch (e) {
+      log(e);
+      throw e;
+    }
+  }
+
+  log('Waiting for office...');
+  Office.onReady(() => {
+    log('Office loaded');
     if (msal.getAccount()) {
       callApi();
     } else if (msal.getLoginInProgress()) {
@@ -53,29 +68,29 @@ Office.onReady(() => {
       loginButton.disabled = false;
       log('Ready. Click button');
     }
-  }
-});
+  });
 
-loginButton.addEventListener('click', async () => {
-  msal.openPopup = () => {
-    const dummy = {
-      close() {
-      },
-      location: {
-        assign(url) {
-          Office.context.ui.displayDialogAsync(url, { width: 25, height: 50 }, res => {
-            dummy.close = res.value.close;
-            res.value.addEventHandler(Office.EventType.DialogMessageReceived, ({ message }) =>
-              dummy.location.href = dummy.location.hash = message
-            );
-          });
+  loginButton.addEventListener('click', async () => {
+    msal.openPopup = () => {
+      const dummy = {
+        close() {
+        },
+        location: {
+          assign(url) {
+            Office.context.ui.displayDialogAsync(url, { width: 25, height: 50 }, res => {
+              dummy.close = res.value.close;
+              res.value.addEventHandler(Office.EventType.DialogMessageReceived, ({ message }) =>
+                dummy.location.href = dummy.location.hash = message
+              );
+            });
+          }
         }
-      }
+      };
+      return dummy;
     };
-    return dummy;
-  };
 
-  log('Logging in...');
-  await msal.loginPopup({ scopes });
-  callApi();
-});
+    log('Logging in...');
+    await msal.loginPopup({ scopes });
+    callApi();
+  });
+})();
